bf7fd52ebc9ab862e9a227da8ea491c786976d5b
Eduardo <tkd13000@gmail.com>
2024-08-15 13:04:55 -0600

    Stars image was added

--- src/CounterDown.jsx
+++ src/CounterDown.jsx
@@ -1,10 +1,14 @@
 import hills from "./assets/pattern-hills.svg"//Importar imagen de hills
+import stars  from "./assets/bg-stars.svg"
 import "./index.css";
 
 function CounterDown()
 {
     return(
-        <img className="hills" src={hills} alt="Not Found" />
+        <>
+            <img className="hills" src={hills} alt="Hills" />
+            <img className="stars" src={stars} alt="Stars"/>
+        </>
     );
 }
 
