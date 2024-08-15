f2d6f35d629eef39c93d2a8135e769a3bf2e83ac
Eduardo <tkd13000@gmail.com>
2024-08-15 13:21:24 -0600

    Icons were added

--- src/CounterDown.jsx
+++ src/CounterDown.jsx
@@ -1,5 +1,8 @@
 import hills from "./assets/pattern-hills.svg"//Importar imagen de hills
 import stars  from "./assets/bg-stars.svg"
+import facebook from "./assets/icon-facebook.svg"
+import instagram from "./assets/icon-instagram.svg"
+import pinterest from "./assets/icon-pinterest.svg"
 import "./index.css";
 
 function CounterDown()
@@ -8,6 +11,11 @@ function CounterDown()
         <>
             <img className="hills" src={hills} alt="Hills" />
             <img className="stars" src={stars} alt="Stars"/>
+            <div className="icon-container">
+            <img className="facebook" src={facebook} alt="Facebook"/>
+            <img className="pinterest" src={pinterest} alt="Pinterest"/>
+            <img className="instagram" src={instagram} alt="Instagram"/>
+            </div>
         </>
     );
 }
